76679b5e794fa81886afc22e5516231700970758
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-01-26 11:00:50 +0100

    refactor: removing python cache

---
+++ code_asm/parser_asm/src/main/c/main.c
@@ -0,0 +1,24 @@
+//
+// Created by bapti on 26/01/2024.
+//
+
+#include "main.h"
+#include <parm.h>
+
+
+void run ()
+{
+BEGIN ();
+int a = 1;
+int b = 2;
+int c = 3;
+int max = a;
+// Found the maximum of 1, 2, 3, using a loop
+for (int i = 0; i < 3; i++) {
+if (max < b) {
+max = b;
+}
+}
+}
+END ();
+}
